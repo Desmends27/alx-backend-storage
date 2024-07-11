093b3313f4204762d4d42233ad04959699bd1534
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-11 19:29:22 +0000

    2022

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- List all bands with Glam rock
 -- Ranked by logevity
 SELECT band_name,
-(split - formed) AS lifespan
+(IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
