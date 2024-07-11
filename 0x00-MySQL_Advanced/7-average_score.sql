526ed92d2d8d23b5d37fd44a4bbb2599f8fee86b
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-11 23:15:08 +0000

    dne

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,16 +7,21 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
 )
 BEGIN
     DECLARE sum_scores INT;
+    DECLARE num_corrections INT;
     DECLARE average FLOAT;
 
     -- Calculate sum of scores and count of corrections for the user
-    SELECT SUM(score) INTO sum_scores FROM corrections WHERE user_id = user_id;
+    SELECT SUM(score), COUNT(*) INTO sum_scores, num_corrections FROM corrections WHERE user_id = user_id;
+
+    -- Calculate average score
+    IF num_corrections > 0 THEN
+        SET average = sum_scores / num_corrections;
+    ELSE
+        SET average = 0;  -- Handle division by zero case
+    END IF;
 
     -- Update users table with the calculated average score
-    UPDATE users SET
-	average = sum_scores / (SELECT COUNT(score) FROM corrections WHERE user_id = users_id)
-	WHERE id = user_id
-
+    UPDATE users SET average_score = average WHERE id = user_id;
 END $$
 
 DELIMITER ;
