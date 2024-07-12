1e8b66e7b9c784c95fb6cdc9ce6c27a8b1307d7c
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-12 23:24:14 +0000

    drop

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,4 @@
 -- Create the index idx_name_first on the 
 -- table names and the first letter of the name
+DROP INDEX idx_name_first ON names;
 CREATE INDEX idx_name_first ON names (LEFT(name, 1));
