e3a914a9defc9f9f4f286744ae53ee95edd586a5
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-12 23:20:53 +0000

    new

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
 -- Create the index idx_name_first on the 
 -- table names and the first letter of the name
-CREATE INDEX idx_name_first ON names(names(1));
+CREATE INDEX idx_name_first ON names (LEFT(name, 1));
