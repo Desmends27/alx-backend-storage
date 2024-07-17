f129ad49da3f35f6f7711b40cae8ef7b4d2f5d13
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-17 02:12:57 +0000

    error in question

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Returns the list of school having a specific topic """
 
+
 def schools_by_topic(mongo_collection, topic):
     """ Functions tha returns the list of school having a topic"""
-    return mongo_collection.find({"topic": topic})
+    return mongo_collection.find({"topics": topic})
