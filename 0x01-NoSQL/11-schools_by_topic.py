f0e7b4cb864a5ec1b57970d56990fecd6f3f61f5
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-17 02:10:31 +0000

    test

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Returns the list of school having a specific topic """
+
+def schools_by_topic(mongo_collection, topic):
+    """ Functions tha returns the list of school having a topic"""
+    return mongo_collection.find({"topic": topic})
