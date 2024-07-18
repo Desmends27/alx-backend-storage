0190ba09d3baf129f2160335c18351c2e8a322f6
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-18 00:29:32 +0000

    mset

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Writing String to redis """
 import uuid
 import redis
-from typing import List
+from typing import Union
 
 
 class Cache:
@@ -15,8 +15,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: List[str, bytes, int, float]) -> str:
+    def store(self, data: Union[int, str, bytes, float]) -> str:
         """ Stores a data in the database"""
         key = str(uuid.uuid4())
-        self._redis.set(key, data)
+        self._redis.mset(key, data)
         return key
