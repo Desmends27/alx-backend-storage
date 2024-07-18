d2e70d5de2adb57f2b4da82cae13cb20708878d2
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-18 00:26:55 +0000

    test

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """ Writing String to redis """
 import uuid
 import redis
+from typing import List
 
 
 class Cache:
@@ -14,7 +15,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: List[str, bytes, int, float]) -> str:
         """ Stores a data in the database"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
